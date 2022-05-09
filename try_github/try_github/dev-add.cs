4c20c613975431b494c4afd223740c150be9ae34
ueharakazuyuki9 <uehara-kazuyuki-2211@occ.co.jp>
2022-05-09 17:37:24 +0900

    足し算

---
+++ try_github/try_github/dev-add.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace try_github
+{
+    class Add
+    {
+
+        public int add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
